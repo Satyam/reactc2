e8d73c074de999c044ba972400d44c820d67546f
Satyam <satyam@satyam.com.ar>
2020-03-19 15:03:29 +0100

    Turn data/sectores into array, have indexed on load

--- src/Store/data/sectores.js
+++ src/Store/data/sectores.js
@@ -1,30 +1,30 @@
-export const sectores = {
-  constitucion: {
+export const sectores = [
+  {
     idSector: 'constitucion',
     descrCorta: 'Constitución',
     descr: 'Estación Constitución, Ciudad de Buenos Aires, Argentina',
     ancho: 11,
     alto: 7,
   },
-  simpleDesvio: {
+  {
     idSector: 'simpleDesvio',
     descrCorta: '1- Desvío Simple',
     descr: 'Una única celda con un cambio y señal',
     ancho: 1,
     alto: 1,
   },
-  cruceDobleCambio: {
+  {
     idSector: 'cruceDobleCambio',
     descrCorta: '2- Doble cambio de vías',
     descr: 'Vías paralelas con cambios cruzados y señales',
     ancho: 2,
     alto: 2,
   },
-  senalesEncadenadas: {
+  {
     idSector: 'senalesEncadenadas',
     descrCorta: '3- Señales encadenadas',
     descr: 'Señal en amarillo cuando la siguiente está en rojo',
     ancho: 4,
     alto: 1,
   },
-};
+];
--- src/Store/sectores/reducer.js
+++ src/Store/sectores/reducer.js
@@ -1,4 +1,15 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { sectores } from 'Store/data';
 
-export default createReducer(sectores || [], {});
+export default createReducer(sectores || [], {
+  '@@INIT': () =>
+    sectores.reduce((ss, s) => {
+      const idSector = s.idSector;
+      if (ss[idSector])
+        throw new Error(`Definición de sector  ${idSector} repetida.`);
+      return {
+        ...ss,
+        [idSector]: s,
+      };
+    }, {}),
+});
