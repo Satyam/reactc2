5a75cce073dd418348009bb87372b192f2ef701a
Satyam <satyam@satyam.com.ar>
2020-03-28 09:13:00 +0100

    Utils/nextCoords also provides entry direction on next celda

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -17,21 +17,21 @@ export const buildId = ({ idSector, x, y, dir }) =>
 export function nextCoords(x, y, dir) {
   switch (dir) {
     case N:
-      return [x, y - 1];
+      return [x, y - 1, S];
     case NE:
-      return [x + 1, y - 1];
+      return [x + 1, y - 1, SW];
     case E:
-      return [x + 1, y];
+      return [x + 1, y, W];
     case SE:
-      return [x + 1, y + 1];
+      return [x + 1, y + 1, NW];
     case S:
-      return [x, y + 1];
+      return [x, y + 1, N];
     case SW:
-      return [x - 1, y + 1];
+      return [x - 1, y + 1, NE];
     case W:
-      return [x - 1, y];
+      return [x - 1, y, E];
     case NW:
-      return [x - 1, y - 1];
+      return [x - 1, y - 1, SE];
     default:
   }
 }
