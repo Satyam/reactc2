621bcb97dc896ad5fa6d0c094f7e57d55f8340c1
Satyam <satyam@satyam.com.ar>
2020-03-31 17:45:25 +0200

    Ensure enclavamientos for Cambio are processed first

--- src/Store/enclavamientos/selectors.js
+++ src/Store/enclavamientos/selectors.js
@@ -1,10 +1,17 @@
 import { createSelector } from '@reduxjs/toolkit';
+import { CAMBIO, SENAL } from 'Store/data';
 
 export const selEnclavamientos = createSelector(
   state => state.enclavamientos,
   (state, idSector) => idSector,
   (enclavamientos, idSector) =>
-    Object.values(enclavamientos).filter(e => e.idSector === idSector)
+    Object.values(enclavamientos)
+      .filter(e => e.idSector === idSector)
+      .sort((a, b) => {
+        if (a.tipo === CAMBIO && b.tipo === SENAL) return -1;
+        if (a.tipo === SENAL && b.tipo === CAMBIO) return 1;
+        return 0;
+      })
 );
 
 export const selEnclavamiento = (state, idOrigen) =>
