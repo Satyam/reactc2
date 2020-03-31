91e98d37dce661e32fc2d23e1e54b24620dd096f
Satyam <satyam@satyam.com.ar>
2020-03-31 15:41:46 +0200

    selSenales returns null instead of empty array

--- src/Store/senales/selectors.js
+++ src/Store/senales/selectors.js
@@ -1,17 +1,21 @@
 import { createSelector } from '@reduxjs/toolkit';
 
 export const selSenal = (state, idSenal) => state.senales[idSenal];
+
 export const selSenalIsManual = (state, idSenal) =>
   state.senales[idSenal].manual;
 
 export const selSenales = createSelector(
   state => state.senales,
   (state, celda) => celda,
-  (senales, celda) =>
-    Object.values(senales).filter(
+  (senales, celda) => {
+    const ss = Object.values(senales).filter(
       senal =>
         senal.idSector === celda.idSector &&
         senal.x === celda.x &&
         senal.y === celda.y
-    )
+    );
+    if (ss.length) return ss;
+    return null;
+  }
 );
