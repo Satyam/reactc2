fff4febdf35803f9e703a77e77c28a6e2e4e6011
Satyam <satyam@satyam.com.ar>
2020-04-04 18:34:41 +0200

    Changed trein color depending on speed. Improved `title` attr. info

--- src/Components/Trenes/Tren.jsx
+++ src/Components/Trenes/Tren.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Train } from 'Components/Icons';
+import { useTren } from 'Store';
 
 import { CENTRO_CELDA } from 'Components/common';
 import styles from './styles.module.css';
 
 export default function Tren({ celda }) {
+  const tren = useTren(celda.idTren);
+  let color = 'gray';
+  switch (tren.speed) {
+    case 0:
+      color = 'red';
+      break;
+    case 1:
+      color = 'black';
+      break;
+    default:
+      break;
+  }
   return (
     <g transform={`translate(${CENTRO_CELDA - 8}, ${CENTRO_CELDA - 8})`}>
       <circle cx={8} cy={8} r={9} className={styles.tren}>
-        <title>{celda.idTren}</title>
+        <title>{`${celda.idTren}\nVelocidad: ${tren.speed}\nFalta: ${tren.falta}`}</title>
       </circle>
-      <Train size="16px" />
+      <Train color={color} size="16px" />
     </g>
   );
 }
