4c0b7aecc83b78547660607896a7d19ee87c2cbf
Satyam <satyam@satyam.com.ar>
2020-04-06 18:46:37 +0200

    Changed the style of tren to show half-speed

--- src/Components/Trenes/Tren.jsx
+++ src/Components/Trenes/Tren.jsx
@@ -7,23 +7,20 @@ import styles from './styles.module.css';
 
 export default function Tren({ celda }) {
   const tren = useTren(celda.idTren);
-  let color = 'gray';
-  switch (tren.speed) {
-    case 0:
-      color = 'red';
-      break;
-    case 1:
-      color = 'black';
-      break;
-    default:
-      break;
-  }
+  const relSpeed = tren.speed / (tren.maxSpeed || 1);
+
   return (
     <g transform={`translate(${CENTRO_CELDA - 8}, ${CENTRO_CELDA - 8})`}>
       <circle cx={8} cy={8} r={9} className={styles.tren}>
         <title>{`${celda.idTren}\nVelocidad: ${tren.speed}\nFalta: ${tren.falta}`}</title>
       </circle>
-      <Train color={color} size="16px" />
+      <path
+        d="M 8 0 A 8 8 90 1 0 8 16 z"
+        className={
+          relSpeed && relSpeed !== 1 ? styles.halfSpeed : styles.transparent
+        }
+      />
+      <Train color={relSpeed ? 'black' : 'red'} size="16px" />
     </g>
   );
 }
