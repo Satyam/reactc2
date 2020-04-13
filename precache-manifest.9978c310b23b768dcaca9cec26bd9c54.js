cfbe8a9565efc82ab4411a5922b50dc23703b09b
Satyam <satyam@satyam.com.ar>
2020-04-13 15:29:22 +0200

    Updates

---
+++ precache-manifest.9978c310b23b768dcaca9cec26bd9c54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc72e53fcf0b67d33a771577b3fb39f2",
+    "url": "/CTC/index.html"
+  },
+  {
+    "revision": "0be8a4296866880511bc",
+    "url": "/CTC/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b222ae0c3a10d418c682",
+    "url": "/CTC/static/css/main.14be834b.chunk.css"
+  },
+  {
+    "revision": "0be8a4296866880511bc",
+    "url": "/CTC/static/js/2.a6f58b7e.chunk.js"
+  },
+  {
+    "revision": "7f36efac70c95fde5c159feb4cd24ff0",
+    "url": "/CTC/static/js/2.a6f58b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b222ae0c3a10d418c682",
+    "url": "/CTC/static/js/main.8e62c202.chunk.js"
+  },
+  {
+    "revision": "96bae6216c7b9cde3e96",
+    "url": "/CTC/static/js/runtime-main.b0d39802.js"
+  }
+]);
