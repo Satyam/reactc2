f59aa3c5ae07d1801f66b92dec52778c675a8883
Satyam <satyam@satyam.com.ar>
2020-04-14 17:33:50 +0200

    Made `setEnclavamientos` use Promises so it solves over various animation frames and avoids taking up too much time uninterrupted.

--- src/Store/enclavamientos/actions.js
+++ src/Store/enclavamientos/actions.js
@@ -15,7 +15,7 @@ import { VERDE, ROJO, CAMBIO, SENAL, BLOQUE, NORMAL, CENTRO } from 'Store/data';
 import { doSetCambio, doSetLuzEstado } from 'Store/actions';
 
 export function setEnclavamientos(idOrigen, tipoOrigen, force) {
-  return (dispatch, getState) => {
+  return async (dispatch, getState) => {
     if (!selEnclavamientosActive(getState())) return;
     let idSector;
     switch (tipoOrigen) {
@@ -45,16 +45,18 @@ export function setEnclavamientos(idOrigen, tipoOrigen, force) {
 
     function enclavamientosCambio(idTarget, dependencias) {
       const celdaTarget = selCelda(getState(), idTarget);
-      return dependencias.reduce((r1, dep) => {
+      return dependencias.reduce((p1, dep) => {
         const idSource = buildId({ idSector, x: dep.x, y: dep.y });
         const celdaSource = selCelda(getState(), idSource);
         const posicionEsperada =
           dep[celdaSource.posicion] ||
           (celdaTarget.tipo === CAMBIO ? NORMAL : CENTRO);
-        if (posicionEsperada === celdaTarget.posicion) return r1;
-        if (selCeldaIsManual(getState(), idTarget)) return r1;
-        return !!dispatch(doSetCambio(idTarget, posicionEsperada)) || r1;
-      }, false);
+        if (posicionEsperada === celdaTarget.posicion) return p1;
+        if (selCeldaIsManual(getState(), idTarget)) return p1;
+        return p1.then(
+          (r1) => !!dispatch(doSetCambio(idTarget, posicionEsperada)) || r1
+        );
+      }, Promise.resolve(false));
     }
 
     function enclavamientosSenal(idTarget, dependencias) {
@@ -106,37 +108,42 @@ export function setEnclavamientos(idOrigen, tipoOrigen, force) {
             );
         }
       });
-      return Object.keys(nuevoEstado).reduce((r2, luz) => {
-        if (!(luz in senalTarget)) return r2;
-        if (senalTarget[luz] === nuevoEstado[luz]) return r2;
+      return Object.keys(nuevoEstado).reduce((p2, luz) => {
+        if (!(luz in senalTarget)) return p2;
+        if (senalTarget[luz] === nuevoEstado[luz]) return p2;
 
-        return (
-          !!dispatch(doSetLuzEstado(idTarget, luz, nuevoEstado[luz])) || r2
+        return p2.then(
+          (r2) =>
+            !!dispatch(doSetLuzEstado(idTarget, luz, nuevoEstado[luz])) || r2
         );
-      }, false);
+      }, Promise.resolve(false));
     }
 
     const browseEnclavamientos = () => {
       const enclavamientos = selEnclavamientos(getState(), idSector);
-      return enclavamientos.reduce((r, encl) => {
+      return enclavamientos.reduce((p, encl) => {
         const { x, y, dir, tipo, dependencias } = encl;
         const idTarget = buildId({ idSector, x, y, dir });
-        if (!force && idTarget === idOrigen) return r;
+        if (!force && idTarget === idOrigen) return p;
         switch (tipo) {
           case CAMBIO:
-            return enclavamientosCambio(idTarget, dependencias) || r;
+            return p.then(
+              (r) => enclavamientosCambio(idTarget, dependencias) || r
+            );
           case SENAL:
-            return enclavamientosSenal(idTarget, dependencias) || r;
+            return p.then(
+              (r) => enclavamientosSenal(idTarget, dependencias) || r
+            );
           default:
             throw new Error(
               `Celda ${idTarget} tiene enclavamiento desconocido ${tipo}`
             );
         }
-      }, false);
+      }, Promise.resolve(false));
     };
 
     let countDown = 9;
-    while (countDown && browseEnclavamientos()) countDown--;
+    while (countDown && (await browseEnclavamientos())) countDown--;
     if (!countDown) throw new Error(`Enclavamiento en loop por ${idOrigen}`);
     return !!countDown;
   };
