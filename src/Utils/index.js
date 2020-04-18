8e1c35026601e102788c1d674dfe05917d32b39f
Satyam <satyam@satyam.com.ar>
2020-04-18 02:21:41 +0200

    Added `useLongPress` click and long press handler

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,4 +1,63 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
+
+export function useLongPress({
+  onClick = () => {},
+  onLongPress = () => {},
+  ms = 300,
+} = {}) {
+  const [startLongPress, setStartLongPress] = useState(false);
+  const timer = useRef(false);
+
+  const callback = useCallback(() => {
+    onLongPress();
+    timer.current = false;
+  }, [onLongPress]);
+
+  useEffect(() => {
+    if (startLongPress) {
+      timer.current = setTimeout(callback, ms);
+    } else {
+      if (timer.current) onClick();
+      timer.current = false;
+    }
+    return () => {
+      clearTimeout(timer.current);
+    };
+  }, [onClick, onLongPress, ms, startLongPress, callback]);
+
+  const start = useCallback(() => {
+    setStartLongPress(true);
+  }, []);
+  const stop = useCallback(() => {
+    setStartLongPress(false);
+  }, []);
+
+  return {
+    onMouseDown: start,
+    onMouseUp: stop,
+    onMouseLeave: stop,
+    onTouchStart: start,
+    onTouchEnd: stop,
+  };
+}
+
+/* Usage:
+
+https://stackoverflow.com/questions/48048957/react-long-press-event
+
+function MyComponent (props) {
+  const longPressProps = useLongPress({
+    onClick: () => void console.log('on click'),
+    onLongPress: () => void console.log('long press'),
+  });
+
+
+  return (
+      <button {...longPressProps} >
+        click me
+      </button>  );
+};
+*/
 
 export const isPlainClick = (ev) => {
   if (ev.button || ev.shiftKey || ev.altKey || ev.metaKey || ev.ctrlKey)
