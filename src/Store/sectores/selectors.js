422ecd92f600da6fa18c0ca2c6550cafae35e597
Satyam <satyam@satyam.com.ar>
2020-04-30 18:36:04 +0200

    Tidyed up sctores/selectors by ordering and spacing

--- src/Store/sectores/selectors.js
+++ src/Store/sectores/selectors.js
@@ -1,10 +1,11 @@
-import { sectorAdapter } from './reducer';
+import sectorAdapter from './adapter';
+
 const selectors = sectorAdapter.getSelectors((state) => state.sectores);
-export const selSectores = (state) => selectors.selectAll(state);
-export const selSector = (state, idSector) =>
-  selectors.selectById(state, idSector);
-export const selSectoresLoading = (state) => state.sectores.loadStatusSectores;
-export const selErrorSectores = (state) => state.sectores.errorSectores;
+
+export const selSector = selectors.selectById;
+export const selSectorLoading = (state) => state.sectores.loadStatusSector;
 export const selErrorSector = (state) => state.sectores.errorSector;
 
-export const selSectorLoading = (state) => state.sectores.loadStatusSector;
+export const selSectores = selectors.selectAll;
+export const selSectoresLoading = (state) => state.sectores.loadStatusSectores;
+export const selErrorSectores = (state) => state.sectores.errorSectores;
